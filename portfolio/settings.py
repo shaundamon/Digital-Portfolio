5d2b6dcc591ef8e15a958b0ea8a3fe878b54281a
Thembisile <shaundamon6@icloud.com>
2023-09-25 13:44:12 +0200

    load envs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -4,6 +4,11 @@ from decouple import config
 import dj_database_url
 
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
